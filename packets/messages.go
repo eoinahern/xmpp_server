f61e80752f2ccfc4739945fc918ba702dd7417e0
eoinahern <eoinpahern@yahoo.co.uk>
2016-10-17 20:02:54 +0100

    minor changes in messages

--- packets/messages.go
+++ packets/messages.go
@@ -24,20 +24,3 @@ type Presence struct {
 	to   string
 	body string
 }
-
-// stream
-
-/*type Stream struct {
-	Inner InnerStream `json:"stream"`
-}
-
-type InnerStream struct {
-	From    string `json:"from"` //myid prob email
-	To      string `json:"to"`
-	Id      string `json:"id"`  //This attribute is a unique identifier created by the receiving entity to function as a session key for the initiating entity's streams with
-	Version string `json:"version"`
-	Lang    string `json:"lang"`
-}*/
-
-type Feature struct {
-}
